becb4a8377fb0b079d3f2777cee62a648cb17150
priyank <priyank@example.com>
2026-05-16 01:24:25 +0530

    Migrated from 'mocha' to 'vitest'

--- test/test.js
+++ test/test.js
@@ -1,12 +1,11 @@
-/* globals describe, it */
-
-import '../index.js';
+import { describe, it, vi } from 'vitest';
 
 describe('package', function() {
     describe('package-json', function() {
         // If there would be an error in import, the code would not reach this point
-        it('should load fine using import', function(done) {
-            done();
+        it('should load fine using import', async function() {
+            vi.spyOn(process, 'exit').mockImplementation(() => undefined);
+            await import('../index.js');
         });
     });
 });
